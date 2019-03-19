91aa5842db806a747aca0d916372e4e0cba7b2fb
Charlie <charliekelly31415@gmail.com>
2019-03-19 17:49:47 -0400

    Create portfolio

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,30 @@
-$(() => {
+$(function() {
+  $('.item article').each(function(){
+    const id = $(this).attr('id')
+    const url = $(this).find('.links a.deployed').attr('href')
+    // console.log('id: ' + id + ', url: ' + url)
+    $(this).find('header').wrap(`<a href="${url}" target="_blank"></a>`)
 
+    $(this).on('mouseover', () => {
+      if ($('#sitePreview').attr('src') !== url) {
+        previewSite(url)
+      }
+    })
+  })
+
+  $('.page-header').on('click', closePreview)
 })
+
+const previewSite = (siteUrl) => {
+  $('.preview').show()
+  $('#sitePreview').attr('src', siteUrl)
+}
+
+const closePreview = () => {
+  $('.preview').hide()
+  $('#sitePreview').attr('src', '')
+}
+
+const getSiteTitle = (siteId) => {
+  return $(siteId).find('strong.title').text()
+}
