80c48204501ef4155b112966716e37300dea63e2
Charlie Kelly <charliekelly31415@gmail.com>
2019-11-12 17:21:56 -0500

    drop unused files

--- assets/scripts/index-mat.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const elems = document.querySelectorAll('.sidenav')
-  const instances = M.Sidenav.init(elems)
-
-  instances[0].open()
-})
